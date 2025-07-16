d88dca147d9546209e7758f953da019f792fccb9
Garima Tiwari <garimatiwari1803@gmail.com>
2025-07-16 13:40:23 +0530

    add recharts dependency and implement salary ranges and key industry trends visualization in DashboardView

--- app/(main)/dashboard/_components/dashboardview.jsx
+++ app/(main)/dashboard/_components/dashboardview.jsx
@@ -1,6 +1,12 @@
 "use client";
 
-import { Brain, BriefcaseIcon, LineChart, TrendingDown, TrendingUp } from "lucide-react";
+import {
+  Brain,
+  BriefcaseIcon,
+  LineChart,
+  TrendingDown,
+  TrendingUp,
+} from "lucide-react";
 import React from "react";
 import { format, formatDistanceToNow } from "date-fns";
 import { Badge } from "@/components/ui/badge";
@@ -14,6 +20,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Progress } from "@/components/ui/progress";
+import { Bar, BarChart, CartesianGrid, Legend, Rectangle, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
 
 const DashboardView = ({ insights }) => {
   const salaryData = insights.salaryRange.map((range) => ({
@@ -65,71 +72,160 @@ const DashboardView = ({ insights }) => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Market Outlook</CardTitle>
-            <OutlookIcon className={`h-4 w-4 ${OutlookColor}`}/>
+            <CardTitle className="text-sm font-medium">
+              Market Outlook
+            </CardTitle>
+            <OutlookIcon className={`h-4 w-4 ${OutlookColor}`} />
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">{insights.marketOutlook}</div>
             <p className="text-xs text-muted-foreground">
-                Next Update {nextUpdateDistance};
+              Next Update {nextUpdateDistance};
             </p>
           </CardContent>
-        
         </Card>
-         <Card>
+        <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Industry Growth</CardTitle>
-           <TrendingUp className="h-4 w-4 text-muted-foreground"/>
+            <CardTitle className="text-sm font-medium">
+              Industry Growth
+            </CardTitle>
+            <TrendingUp className="h-4 w-4 text-muted-foreground" />
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold">{insights.growthRate.toFixed(1)}%</div>
-            <Progress value={insights.growthRate} className="mt-2"/>
+            <div className="text-2xl font-bold">
+              {insights.growthRate.toFixed(1)}%
+            </div>
+            <Progress value={insights.growthRate} className="mt-2" />
           </CardContent>
-        
         </Card>
-         <Card>
+        <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Demand Level</CardTitle>
-            <BriefcaseIcon className="h-4 w-4 text-muted-foreground"/>
-           
+            <BriefcaseIcon className="h-4 w-4 text-muted-foreground" />
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">{insights.demandLevel}</div>
-            <div className={`h-2 w-full rounded-full mt-2 ${getDemandLevelColor(
-                             insights.demandLevel
-
-
-                )}`}>
-
-
-
-
-                
-            </div>
+            <div
+              className={`h-2 w-full rounded-full mt-2 ${getDemandLevelColor(
+                insights.demandLevel
+              )}`}
+            ></div>
           </CardContent>
-        
         </Card>
-         <Card>
+        <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Top Skills</CardTitle>
-            <Brain className="h-4 w-4 text-muted-foreground"/>
-            
+            <Brain className="h-4 w-4 text-muted-foreground" />
           </CardHeader>
-          <CardContent>
-            <div className="flex flex-wrap gap-1">
-                {insights.TopSkills.map((skill)=>(
-                    <Badge key={skill} variant="secondary">
-                        {skill}
-                    </Badge>
-
-                )
-
-                )}
+          <CardContent >
+            <div className="flex flex-wrap overflow-auto px-2">
+              {insights.TopSkills.map((skill) => (
+                <Badge className="flex flex-wrap border-white" key={skill} variant="secondary">
+                  {skill}
+                </Badge>
+              ))}
             </div>
-            
           </CardContent>
-        
         </Card>
+      </div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Salary Ranges by Role</CardTitle>
+          <CardDescription>
+            Displaying minimum, median, and maximum salaries (in thousandds)
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="h-[400px]">
+            <ResponsiveContainer width="100%" height="100%">
+              <BarChart
+                
+                data={salaryData}
+                
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip content={({active,payload,label})=>{
+                  if(active && payload && payload.length){
+                    return(
+                      <div className="bg-background border rounder-lg p-2 shadow-md">
+                        <p className="font-medium">{label}</p>
+                        {payload.map((item)=>(
+                          <p key={item.name} className="text-sm">
+                            {item.name}:${item.value}K
+                          </p>
+
+                        ))}
+                      </div>
+                    );
+                  }
+                  return null;
+                }} />
+              
+                <Bar
+                  dataKey="min"
+                  fill="#8884d8"
+                 name="Min Salary" />
+              
+                <Bar
+                  dataKey="median"
+                  fill="#82ca9d"
+                  name="Median Salary"
+                />
+                
+                <Bar
+                  dataKey="max"
+                  fill="gray"
+                  name="Max Salary"
+                />
+              </BarChart>
+            </ResponsiveContainer>
+          </div>
+        </CardContent>
+      </Card>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <Card>
+        <CardHeader>
+          <CardTitle>Key Industry Trends</CardTitle>
+          <CardDescription>
+            Current Trends Shaping the industry
+            
+           </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <ul className="space-y-4">
+            {insights.keyTrends.map((trend,index)=>(
+              <li key={index} className="flex items-start space-x-2">
+                <div className="h-2 w-2 mt-2 rounded-full bg-primary"/>
+                <span>{trend}</span>
+              </li>
+            ))}
+          </ul>
+
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Recommended Skills</CardTitle>
+          <CardDescription>
+            Skills to develop for excelling
+            
+           </CardDescription>
+        </CardHeader>
+        <CardContent>
+           <div className="flex flex-wrap gap-2">
+            {insights.recomendedSkills.map((skill)=>(
+              <Badge key={skill} variant="outline">
+                {skill}
+              </Badge>
+            ))}
+          </div>
+         
+
+
+        </CardContent>
+      </Card>
       </div>
     </div>
   );
